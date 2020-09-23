bfc320478aebffc034dc8144dadfcb2803a50d22
xenolito <oscar.rey.tajes@gmail.com>
2020-09-23 20:40:37 +0200

    modularizing

---
+++ src/default_scene.js
@@ -0,0 +1,106 @@
+//import { Engine } from "@babylonjs/core/Engines/engine"; // Imported automatically for all modules via WEBPACK's ProvidePlugin
+//import { Scene } from "@babylonjs/core/scene"; // Imported automatically for all modules via WEBPACK's ProvidePlugin
+//import { Vector3 } from "@babylonjs/core/Maths/math"; // Imported automatically for all modules via WEBPACK's ProvidePlugin
+
+//import "@babylonjs/core/Debug/debugLayer";
+//import "@babylonjs/inspector";
+
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { Mesh } from "@babylonjs/core/Meshes/mesh";
+import { GridMaterial } from "@babylonjs/materials/grid";
+/*import {
+  Color3,
+  Color4,
+  Vector2,
+  Vector3,
+  Plane,
+} from "@babylonjs/core/Maths/math";*/
+
+// Required side effects to populate the Create methods on the mesh class. Without this, the bundle would be smaller but the createXXX methods from mesh would not be accessible.
+import "@babylonjs/core/Meshes/meshBuilder";
+import { Color4, Color3 } from "@babylonjs/core/Maths/math.color";
+
+export default class DefaultScene {
+  constructor(canvasID, options) {
+    this._canvas = document.getElementById(canvasID);
+    this._engine = new Engine(this._canvas, true, null, true);
+    window._engine = this._engine; // learn how to do this properly without referencing on window
+    this._fpsUI = options?.showFPS && this.buildFPSui();
+    this._defCamera = options?.camera;
+    this._debugUI = options?.debug && this.showDegugUI();
+
+    this.createScene();
+  }
+
+  createScene() {
+    this._scene = new Scene(this._engine);
+    this._scene.clearColor = Color4.FromColor3(Color3.Random());
+    this._defCamera = this.createCamera();
+    this.createLight();
+    this.doRender();
+    this.resize();
+  }
+
+  createCamera() {
+    if (!this._defCamera) {
+      this._defCamera = new ArcRotateCamera(
+        "Camera",
+        0,
+        0,
+        15,
+        new Vector3(0, 0, 0),
+        this._scene,
+        true
+      );
+    }
+    this._defCamera.setPosition(new Vector3(0, 5, 10));
+    this._defCamera.attachControl(this._canvas, true);
+    this._defCamera.setTarget(Vector3.Zero());
+    return this._defCamera;
+  }
+
+  createLight() {
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    this._defLight = new HemisphericLight(
+      "light1",
+      new Vector3(0, 1, 0),
+      this._scene
+    );
+    this._defLight.intensity = 0.7;
+    return this._defLight;
+  }
+
+  doRender() {
+    //console.log(`Checking renderLoop= ${this._engine.runRenderLoop}`);
+    this._engine.runRenderLoop(() => {
+      //console.log("RENDERIZA COJONES " + this._scene.render);
+      this._scene.render();
+      if (this._fpsUI)
+        this._fpsUI.innerHTML = this._engine.getFps().toFixed() + " fps";
+
+      //divFps.innerHTML = engine.getFps().toFixed() + " fps";
+    });
+  }
+
+  buildFPSui() {
+    let uid = "fps_" + Date.now();
+    let fpsel = document.createElement("div");
+    fpsel.id = uid;
+    fpsel.innerHTML = "0";
+    document.body.appendChild(fpsel);
+    return document.getElementById(uid);
+  }
+
+  showDegugUI() {
+    //this._scene.debugLayer.show();
+  }
+
+  resize() {
+    //console.log(window.innerWidth);
+    window.addEventListener("resize", function () {
+      this._engine.resize();
+      console.log(`Ventana redimensionada a ancho: ${window.innerWidth}`);
+    });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Mesh } from "@babylonjs/core/Meshes/mesh";
 import { GridMaterial } from "@babylonjs/materials/grid";
 // Required side effects to populate the Create methods on the mesh class. Without this, the bundle would be smaller but the createXXX methods from mesh would not be accessible.
 import "@babylonjs/core/Meshes/meshBuilder";
-
+import { Color4, Color3 } from "@babylonjs/core/Maths/math.color";
 //import "@babylonjs/loaders";
 import { GLTFLoader } from "@babylonjs/loaders/glTF/2.0/glTFLoader";
 import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
@@ -17,74 +17,25 @@ import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
 //Meshes to load config object
 import { Meshes } from "./gltf_models";
 
+/***  Own components  ***/
+
+import DefaultScene from "./default_scene";
+
 /*** CSS ***/
 import "./css/global.scss";
 import "./css/layouts.scss";
 
-/*------------------------------------------------------------------------------------------------------*
-                        DEGUG LIBRARIES
-\*------------------------------------------------------------------------------------------------------*/
-/*
-import "@babylonjs/core/Debug/debugLayer";
-import "@babylonjs/inspector";
-*/
-
 (function () {
-  let divFps = document.getElementById("fps");
-  // Get the canvas element from the DOM.
-  const canvas = document.getElementById("renderCanvas");
-
-  // Associate a Babylon Engine to it.
-  const engine = new Engine(canvas);
-
-  // Render every frame
-  engine.runRenderLoop(() => {
-    scene.render();
-    divFps.innerHTML = engine.getFps().toFixed() + " fps";
+  let myScene = new DefaultScene("renderCanvas", {
+    showFPS: true, // boolean
+    camera: null, // camera obj.
+    debug: true, // boolean
   });
 
-  window.addEventListener("resize", function () {
-    engine.resize();
-  });
-
-  // Create our first scene.
-  var scene = new Scene(engine);
-
-  // Parameters: alpha, beta, radius, target position, scene
-  var camera = new ArcRotateCamera(
-    "Camera",
-    0,
-    0,
-    15,
-    new Vector3(0, 0, 0),
-    scene,
-    true
-  );
-
-  // Positions the camera overwriting alpha, beta, radius
-  camera.setPosition(new Vector3(0, 5, 10));
-
-  // This attaches the camera to the canvas
-  camera.attachControl(canvas, true);
-
-  /*
-    // This creates and positions a free camera (non-mesh)
-var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-
-// This targets the camera to scene origin
-camera.setTarget(Vector3.Zero());
-
-// This attaches the camera to the canvas
-camera.attachControl(canvas, true);
-*/
-  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-  var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
-
-  // Default intensity is 1. Let's dim the light a small amount
-  light.intensity = 0.7;
+  //  myScene.createScene();
 
   // Create a grid material
-  var material = new GridMaterial("grid", scene);
+  //var material = new GridMaterial("grid", scene);
 
   /*
     // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
@@ -100,8 +51,6 @@ camera.attachControl(canvas, true);
 
   // Affect a material
   //ground.material = material;
-
-  //scene.debugLayer.show();
 
   /*
     Promise.all([
@@ -148,35 +97,13 @@ camera.attachControl(canvas, true);
   ];
 
   let loadMesh = loadMeshGltf(lastMesh);
-  /*let loadMesh = loadMeshGltf(
-    gltfMeshesToTest.meshes[gltfMeshesToTest.meshes.length]
-  );*/
-
-  /*------------------------------------------------------------------------------------------------------*
-                            SHOW DEBUG LAYER
-    \*------------------------------------------------------------------------------------------------------*/
-  //scene.debugLayer.show();
-
-  /*
-    SceneLoader.ImportMeshAsync(null, assetsBaseURL + "sans/", "scene.gltf", scene).then(function (
-        result
-    ) {
-        result.meshes[0].position.x = -0.5;
-        result.meshes[0].position.y = 1;
-        result.meshes[0].scaling.scaleInPlace(1);
-        camera.setTarget(result.meshes[0], false, false);
-        camera.allowUpsideDown = false;
-        console.log("mesh loaded");
-        return result.meshes[0];
-    });
-    */
 
   function loadMeshGltf(meshKeyName) {
     SceneLoader.ImportMeshAsync(
       null,
       Meshes.baseURL + Meshes.meshes[meshKeyName].dir + "/",
       Meshes.meshes[meshKeyName].mesh,
-      scene
+      myScene._scene
     ).then(function (result) {
       result.meshes[0].position.x =
         Meshes.meshes[meshKeyName].position.x || -0.5;
@@ -186,20 +113,20 @@ camera.attachControl(canvas, true);
       result.meshes[0].scaling.scaleInPlace(
         Meshes.meshes[meshKeyName].scale || 1
       );
-      camera.setTarget(result.meshes[0], false, false);
-      camera.allowUpsideDown = false;
+      myScene._defCamera.setTarget(result.meshes[0], false, false);
+      myScene._defCamera.allowUpsideDown = false;
 
-      camera.lowerBetaLimit = 0.1;
-      camera.upperBetaLimit = (Math.PI / 2) * 1.1;
-      camera.lowerRadiusLimit = 3;
-      camera.upperRadiusLimit = 45;
+      myScene._defCamera.lowerBetaLimit = 0.1;
+      myScene._defCamera.upperBetaLimit = (Math.PI / 2) * 1.1;
+      myScene._defCamera.lowerRadiusLimit = 3;
+      myScene._defCamera.upperRadiusLimit = 45;
       // camera sensitivity
       //sensitivity
       //camera.angularSensibilityX = 4100;
       //camera.angularSensibilityY = 6500;
       //camera.panningSensibility = 100;
       //camera.speed = 0.5;
-      camera.pinchPrecision = 50.0;
+      myScene._defCamera.pinchPrecision = 50.0;
       //camera.pinchDeltaPercentage = 800;
 
       return result.meshes[0];
